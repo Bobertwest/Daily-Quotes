c0fd3b7f13207a18a34d9bdea7e34e58c73c83c5
Robert West <robertmathewwest10@gmail.com>
2021-09-03 12:06:22 -0500

    fondo que cambia en base a la hora

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "../src/Styles/App.css";
 import Frase from "./Components/Frase";
+import { fondo } from "./helpers/helpers";
 
 function App() {
   const [frase, setFrase] = useState({});
+  const [hora, setHora] = useState(0);
   const [load, setLoad] = useState(false);
 
   const getPrhase = async () => {
+    setHora(parseInt(new Date().getHours()));
     setLoad(true);
     const api = await fetch("https://type.fit/api/quotes");
     const response = await api.json();
@@ -20,11 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      <img
-        src="https://i0.wp.com/medioambienteynaturaleza.com/wp-content/uploads/2015/06/Fondos-de-pantalla-de-paisajes-naturales25.jpg?ssl=1"
-        alt="fondo de pagina"
-        className="fondo"
-      />
+      <img src={fondo(hora)} alt="fondo de pagina" className="fondo" />
       <Frase getPrhase={getPrhase} frase={frase} load={load} />
     </div>
   );
---
+++ src/helpers/helpers.js
@@ -0,0 +1,34 @@
+export const fondo = (hora) => {
+  switch (hora) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      return "http://www.todopaisajes.com/1366x768/noche-lunar.jpg";
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+      return "https://i0.wp.com/medioambienteynaturaleza.com/wp-content/uploads/2015/06/Fondos-de-pantalla-de-paisajes-naturales25.jpg?ssl=1";
+    case 17:
+    case 18:
+      return "https://s1.1zoom.me/big0/417/423475-Kycb.jpg";
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+      return "http://www.todopaisajes.com/1366x768/noche-lunar.jpg";
+    default:
+      break;
+  }
+};
